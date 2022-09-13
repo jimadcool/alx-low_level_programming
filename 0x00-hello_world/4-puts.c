c4471d7c993199adb432d9b1f10786540feded39
jimadcool <maryam.omoyele3@gmail.com>
2022-09-12 23:33:21 -0700

    saleem

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 /**
+ * main - Entry point
  *
- *  *main - Entry point
- *
- *   *
- *
- *    *Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
+	char low, e, q;
+	e = 'e';
+	q = 'q';
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	printf("\n");
 	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
