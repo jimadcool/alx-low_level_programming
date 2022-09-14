d6c02011a89da5655439ad3ddebb3411b89a203e
jimadcool <maryam.omoyele3@gmail.com>
2022-09-14 13:32:58 -0700

    who go help

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,3 +10,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
+}
