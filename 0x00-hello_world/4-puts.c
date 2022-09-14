822aa2e3201fa9fa1c7adcaf811401e6237c57bf
jimadcool <maryam.omoyele3@gmail.com>
2022-09-14 06:34:50 -0700

    somee

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,43 @@
-#inclde <stdio.h>
+#include &lt;stdlib.h&gt;
+
+#include &lt;stdlib.h&gt;
+
+#include &lt;time.h&gt;
 
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
+
+	char low, e, q;
+
+	e = &#39;e&#39;;
+
+	q = &#39;q&#39;;
+
+	for (low = &#39;a&#39;; low &lt;= &#39;z&#39;; low++)
+
+	{
+
+		if (low != e &amp;&amp; low != q)
+
+			putchar(low);
+
+
+
+	}
+
+	printf(&quot;\n&quot;);
+
 	return (0);
+
 }
