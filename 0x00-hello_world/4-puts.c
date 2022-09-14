47635ce89b1a978562b79b5d652d3714e85d5dba
jimadcool <maryam.omoyele3@gmail.com>
2022-09-14 07:59:01 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,11 @@
-#include &lt;stdlib.h&gt;
-#include &lt;stdlib.h&gt;
-#include &lt;time.h&gt;
-/*
+#include <stdio.h>
+/** 
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char low, e, q;
-
-	e = &#39;e&#39;;
-	q = &#39;q&#39;;
-
-	for (low = &#39;a&#39;; low &lt;= &#39;z&#39;; low++)
-	{
-		if (low != e &amp;&amp; low != q)
-		putchar(low);
-	}
-	printf(&quot;\n&quot;);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
