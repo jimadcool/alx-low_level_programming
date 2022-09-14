556156b4d4eec4ebcbca97014363453dc1ae7118
jimadcool <maryam.omoyele3@gmail.com>
2022-09-14 07:15:20 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,17 +6,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	char low, e, q;
 
-	e= &#39;e&#39;;
+	e = &#39;e&#39;;
 	q = &#39;q&#39;;
 
 	for (low = &#39;a&#39;; low &lt;= &#39;z&#39;; low++)
 	{
 		if (low != e &amp;&amp; low != q)
-			putchar(low);
+		putchar(low);
 	}
 	printf(&quot;\n&quot;);
 	return (0);
