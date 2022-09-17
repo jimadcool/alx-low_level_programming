98e8946cdac514e173ee38366ac87a51f2621116
jimadcool <maryam.omoyele3@gmail.com>
2022-09-17 12:04:19 -0700

    snake

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	printf("\n");
+
+	return (0);
+}
