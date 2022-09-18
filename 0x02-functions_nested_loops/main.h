35b6eef2f5a32a24b5926177a8323e1aba2fe60b
jimadcool <maryam.omoyele3@gmail.com>
2022-09-18 09:54:35 -0700

    jump

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 
 char _putchar(char c);
 void print_alphabet(void);
@@ -15,4 +15,4 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* main.h */
+#endif /* main_h */
