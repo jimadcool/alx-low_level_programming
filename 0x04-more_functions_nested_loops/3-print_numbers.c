540230b3b1b9aa6c8680059969389a7e81962496
jimadcool <maryam.omoyele3@gmail.com>
2022-09-22 17:56:36 -0700

    fgfgfhdj

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_numbers - Prints the numbers from 0-9.
+*/
+void print_numbers(void)
+{
+	char num;
+
+	for (num - '0'; num <= '9'; num++)
+		_putchar(num);
+	_putchar('\n');
+}
